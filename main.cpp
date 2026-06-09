b8a49e529cdacaf4e30907ec511483b7f1d9f320
Kyle Smith <kyle@cabotmc.dev>
2026-06-09 16:12:31 -0300

    simplify

--- main.cpp
+++ main.cpp
@@ -1,28 +1,15 @@
 #include <iostream>
 
+#include "Colors.h"
 #include "Engine.h"
 #include "GLMath.h"
 #include "3d/actors/StaticMeshActor.h"
-#include "3d/components/DirectionalLightComponent.h"
+#include "3d/actors/DirectionalLightActor.h"
 
 using namespace glengine;
 using namespace glengine::world;
 using namespace glengine::world::components;
 using namespace glengine::world::actors;
-
-// Use for lighting, skybox, world geometry, etc
-class Environment : public Actor {
-public:
-    Environment() {
-        auto sun = CreateComponent<DirectionalLightComponent>();
-        sun->GetTransform()->SetRotation({-3 * PI / 4, PI/4, PI/8});
-
-        sun->Ambient = float4(0.1, 0.1, 0.1, 1);
-        sun->Diffuse = float4(1, 1, 1, 1);
-    }
-
-    void Update(double deltaTime) override {};
-};
 
 int main() {
     auto engine = new Engine("GLEngine Sample", int2(1280, 720));
@@ -30,14 +17,18 @@ int main() {
 
     // add sphere
     auto sphere = engine->SpawnActor<StaticMeshActor>("/builtin/models/sphere.obj");
+    sphere->Material->Diffuse = Colors::BLUE;
     sphere->GetTransform()->SetPosition({1.5, 0, 5});
 
     // add cube
     auto cube = engine->SpawnActor<StaticMeshActor>("/builtin/models/cube.obj");
+    cube->Material->Diffuse = Colors::RED;
     cube->GetTransform()->SetPosition({-1.5, 0, 5});
 
     // add environment
-    engine->SpawnActor<Environment>();
+    auto sun = engine->SpawnActor<DirectionalLightActor>();
+    sun->Ambient = float4(0.1, 0.1, 0.1, 1);
+    sun->GetTransform()->SetRotation({-3 * PI / 4, PI/4, PI/8});
 
     engine->MainLoop();
     return 0;
